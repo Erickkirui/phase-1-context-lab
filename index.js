cda752ec099ae254bdeec8a6e57e4cda4fc43bbf
Erickkirui <erickkirui653@gmail.com>
2023-04-05 13:41:36 +0300

    createdEmployee record

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 /* Your Code Here */
+// create employeees records
+function createEmployeeRecord(employeeInfo) {
+    const employee = {};
+    employee.firstName = employeeInfo[0];
+    employee.familyName = employeeInfo[1];
+    employee.title = employeeInfo[2];
+    employee.payPerHour = employeeInfo[3];
+    employee.timeInEvents = [];
+    employee.timeOutEvents = [];
+    return employee;
+  }
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
